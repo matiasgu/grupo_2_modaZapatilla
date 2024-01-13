01b024a30c28d37b8c1c83c51fab3062c157a369
franco <franco.13.@gmail.com>
2024-01-12 21:37:36 -0300

    agregue formulario login(html y css)tambien modifique app,js

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ app.listen(3000, () => console.log('Server running at http://localhost:3000'));
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
