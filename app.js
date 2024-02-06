6ff99b804ef3717c89ffbf95618fbee9fb8da40d
matiasgu <matias.gutierrez4989@gmail.com>
2024-02-06 17:33:52 -0300

    arregle las rutas

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 /* agregue esto al app para mi register.html */
-app.get('/register.html', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
 /*Product Detail*/ 
-app.get('/productDetail.html', (req, res) => {
+app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'));
 });
