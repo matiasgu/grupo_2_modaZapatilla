29e1dff3e65b3bfa48417e87419bade651ab57cf
Carla Vidal <carlamv221@gmail.com>
2024-02-05 21:16:39 -0300

    index por home

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 /* agregue esto al app para mi register.html */
 app.get('/register.html', (req, res) => {
