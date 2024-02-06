1015109924c2534bb4304cd2f490cd68ff6ec9ee
matiasgu <matias.gutierrez4989@gmail.com>
2024-02-06 18:47:25 -0300

    installacion plantilla ejs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.set("view","ejs")// inicializacionde plantilla ejs
+
 app.use(express.static('./public'));
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
