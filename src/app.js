0c3db4d1fac1fa4da972979786932ff1e5ccfea3
Carla Vidal <carlamv221@gmail.com>
2024-03-26 18:10:47 -0300

    product CRUD en proceso

--- src/app.js
+++ src/app.js
@@ -1,62 +1,60 @@
+// ************ Require's ************
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
 const express = require('express');
-const app = express();
+const logger = require('morgan');
 const path = require('path');
-const methodOverride = require('method-override');
+const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
 
-//EJS
-app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, '/views'));
+// ************ express() - (don't touch) ************
+const app = express();
+
+// ************ Middlewares - (don't touch) ************
+app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
+app.use(express.urlencoded({ extended: false })); //para poder utilizar body
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method')); // Para poder pisar el method="POST" en el formulario por PUT y DELETE
+
+// ************ Template Engine - (don't touch) ************
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
+
 
 
 // ************ WRITE YOUR CODE FROM HERE ************
 // ************ Route System require and use() ************
-const homeRouter = require('./routes/homeRoutes'); // Rutas main
-//const productsRouter = require('./routes/products'); // Rutas /products
+const mainRoute = require('./routes/main'); // Rutas main
+const productsRoutes = require('./routes/products'); // Rutas /products
+const usersRoutes = require('./routes/users'); // Rutas /products
 
-app.use('/', homeRouter);
-//app.use('/products', productsRouter);
+app.use('/', mainRoute);
+app.use('/products', productsRoutes);
+app.use('/users', usersRoutes);
 
-
-//******PUT Y DELETE**********/
-app.use(methodOverride('_method'));
-
-
-
-
-// // home
-// app.get('/', (req, res) =>{
-//        res.render("home");
-//  });
-
-// /* agregue esto al app para mi register */
-//  app.get('/register', (req, res) => {
-//       res
-//       .render("register");
-// });
-
-// /*Product Detail*/
-// app.get('/productDetail', (req, res) => {
-//       res.render("productDetail");
-//   });
-
-// //   login
-//   app.get('/login', (req, res) => {
-//       res.render("login");
-//   });
-
-// // producto cart
-//   app.get('/productCart', (req, res) => {
-//       res.render("productCart");
-//   });
-// // producto load
-// app.get('/productLoad', (req, res) => {
-//   res.render("productLoad");
-// });
-  
-
+/////**********OTHERS*********
 app.use(express.static('./public'));
-
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+
+// ************ DON'T TOUCH FROM HERE ************
+// ************ catch 404 and forward to error handler ************
+app.use((req, res, next) => next(createError(404)));
+
+// ************ error handler ************
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.path = req.path;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// ************ exports app - dont'touch ************
+module.exports = app;
 
 
 
