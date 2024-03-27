85f64f67fb565a113260ccffa39005665d740734
Carla Vidal <carlamv221@gmail.com>
2024-03-27 16:32:53 -0300

    edicion del productsController

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -25,20 +25,55 @@ const controller = {
 	// Create -  Method to store
 	store: (req, res) => {
 		// Do the magic
+        const newProduct = {
+            id: crypto.randomUUID(),
+            ...req.body
+        };
+
+        // JS
+        products.push(newUser);
+
+        // JSON
+        fs.writeFileSync( pathFile, JSON.stringify(products, null, 2 ) ) // JS a JSON
+
+        res.redirect('/')		
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		res.render('product-edit-form');
+		// Do the magic
+        const idFound = req.params.id;
+
+        const productFound = products.find( ( product ) => product.id === idFound );
+
+		res.render('product-edit-form'); //hacer el ejs
 	},
 	// Update - Method to update
 	update: (req, res) => {
 		// Do the magic
+        const idFound = req.params.id;
+
+        const indexFound = products.findIndex( ( product ) => product.id === idFound );
+
+        products[indexFound] = {
+            id: products[indexFound].id,
+            ...req.body,
+            // name: req.body.name,
+	    	// image: req.body.image,
+            // brand: req.body.brand,
+            // category: req.body.category,
+            // price: req.body.price,						
+        }
+
+        fs.writeFileSync( pathFile, JSON.stringify(products, null, 2 ) ); // JS a JSON
+
+        res.redirect('/');
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
 		// Do the magic
+
 	}
 };
 
