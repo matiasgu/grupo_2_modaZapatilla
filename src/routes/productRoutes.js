a4ea42c37585d9502494b54f2dfff82d3bf55215
Carla Vidal <carlamv221@gmail.com>
2024-04-10 21:35:57 -0300

    store

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -10,7 +10,7 @@ router.get('/', productsController.index);
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create/', productsController.create); 
-router.post('/', productsController.store); 
+router.post('/store/', productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
