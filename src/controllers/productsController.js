24fb72fc969939895d2ffc92f1578bc5a870aa6a
Carla Vidal <carlamv221@gmail.com>
2024-04-14 17:35:13 -0300

    modificaciones para que funcione el product create

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -30,9 +30,9 @@ const controller = {
 	// Create -  Method to store
 	store: (req, res) => {
 		// Do the magic
-		console.log("estoy en post create" + req.body);
+		console.log(req.body);
         const newProduct = {
-            id: "products.length+1",
+            id: products.length + 1,
 			name: req.body.name,
 			img: req.body.img,
 			brand: req.body.brand,
