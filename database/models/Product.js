c283a4acf481ac021277e676c4ee045dbff307b5
Carla Vidal <carlamv221@gmail.com>
2024-05-25 12:07:07 -0300

    Product.js

---
+++ database/models/Product.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, dataTypes) => {
+let alias = "Productos";
+let cols = {
+    product_id: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    }, 	
+    product_name: {
+        type: dataTypes.STRING
+    },
+    product_brand: {
+        type: dataTypes.STRING
+    },
+    product_detail: {
+        type: dataTypes.STRING
+    },
+    product_category: {
+        type: dataTypes.STRING
+    },
+    product_price: {
+        type: dataTypes.FLOAT
+    },
+    product_discount: {
+        type: dataTypes.INTEGER
+    },
+    product_img: {
+        type: dataTypes.STRING
+    }
+};
+let config = {
+    tableName : "moda_zapatillas",
+    timestamps : false
+
+}
+const Producto = sequelize.define(alias, cols, config);
+return Producto;
+}
