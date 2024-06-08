c610850f765a902861d580c9eda3fd304777701e
claudio peñalva <claudiosaldia777@gmail.com>
2024-06-08 08:02:01 -0300

    login

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -10,7 +10,7 @@ let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const db = require('../database/models');
 const sequelize = db.sequelize;
 /* llamo a los modelos creados */
-const Users = db.User;
+const User = db.User;
 const Op = db.Sequelize.Op;
 //const Products = db.Product;
 //const ShopCarts = db.ShopCart; 
@@ -19,12 +19,12 @@ const controller = {
     // Formulario para iniciar sesión
     login: (req, res) => {
         console.log('estoy por loguearme');
-        res.render('login'); // Renderiza la vista de inicio de sesión
+        res.render('users/login'); // Renderiza la vista de inicio de sesión
     },
 
     // Formulario para registrar usuario
     register: (req, res) => {
-        res.render('register'); // Renderiza la vista de registro de usuario
+        res.render('users/register'); // Renderiza la vista de registro de usuario
     },
 
     /* -----------------------BASE DE DATO -------------------------------- */
@@ -70,41 +70,46 @@ const controller = {
         }
     },
     // Procesar el inicio de sesión
-    processLogin: (req, res) => {
+    
+    processLogin: async(req, res) => {
         let errors = validationResult(req);
-        if (errors.isEmpty()) {
-            console.log(req.body);
-            db.User.findOne({
-                where: {
-                    [Op.and]:[
-                        {
-                            email : req.body.email
-                        },
-                        {
-                            password : req.body.password
-                        }
-                    ]}
-            }).then(function(usuario) {
-                console.log(usuario);
-                if (usuario != null ) {
-                    console.log('se encontro el usuario');
-                    req.session.usuarioLogueado = usuario;
-                    return res.redirect('/');
-                }else{
-                    console.log('no existe usuario');
-                    req.session.usuarioLogueado = undefined;
-                    return res.render('login', {
-                        errors: [{ msg: 'Credenciales inválidas' }]
-                    });  
-                }
-            })
-        } else {
-            console.log('Hay errores');
-            return res.render('login', {
-                errors: errors.errors
-            });
+        // Busca al usuario por el campo 'email'
+        let userToLogin = await User.findOne({ where: { email: req.body.email } });
+
+        //  usuario existe? y si la contraseña es correcta
+    if (userToLogin && bcrypt.compareSync(req.body.password, userToLogin.dataValues.password)) {
+        // Elimina el campo 'password' del objeto usuario para mayor seguridad
+        delete userToLogin.dataValues.password;
+
+        // Almacena la información del usuario en la sesión
+        req.session.userLogged = userToLogin;
+
+        // Si la opción de "remember" está activada, crea una cookie
+        if (req.body.remember) {
+            res.cookie('userEmail', req.body.email, { maxAge: 60000 });// 1 min
         }
+
+        // Redirige al usuario a la home
+        return res.redirect('/');
+    } else {
+        // Si la autenticación falla, renderiza la vista de login con errores y datos antiguos
+        return res.render('login', { errors: errors.mapped(), oldData: req.body });
     }
+},
+profile: (req, res) => {
+    // Renderiza la vista 'userProfile' y pasa el usuario logueado como variable
+    return res.render('userProfile', { user: req.session.userLogged });
+},
+logout: (req, res) => {
+    // Borra la cookie 'userEmail'
+    res.clearCookie('userEmail');
+
+    // Destruye la sesión del usuario
+    req.session.destroy();
+
+    // Redirige al usuario a la página principal
+    return res.redirect('/');
+}
 
 }
 module.exports = controller;
